bbe613239cf5b42ecc9f076930a0d30c7a4235da
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2013-10-10 22:26:35 +0100

    Update facts.rkt

--- private/facts.rkt
+++ private/facts.rkt
@@ -1,6 +1,6 @@
 #lang racket
 ;;; PLT Scheme Inference Collection
-;;; facts.ss
+;;; facts.rkt
 ;;; Copyright (c) 2006-2010 M. Douglas Williams
 ;;;
 ;;; This library is free software; you can redistribute it and/or 
